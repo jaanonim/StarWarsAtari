9efd852fabcc20a28e6f95be9307248336d54675
Jaanonim <mat8mro@gmail.com>
2022-11-18 22:36:34 +0100

    small fix

--- src/GameObjects/Star.ts
+++ src/GameObjects/Star.ts
@@ -4,13 +4,18 @@ import Importer from "3d-game-engine-canvas/src/tools/Importer";
 import TextureLoader from "3d-game-engine-canvas/src/tools/TextureLoader";
 import Color from "3d-game-engine-canvas/src/utilities/math/Color";
 import Vector3 from "3d-game-engine-canvas/src/utilities/math/Vector3";
+import Texture from "3d-game-engine-canvas/src/utilities/Texture";
 import { StarComp } from "../Components/StarComp";
 
-const tex = new TextureLoader(
-    await FileLoader.loadImg("img/nothing.png")
-).parse();
+let tex: Texture;
 
 export default async function Star(pos: Vector3, dir: Vector3, color: Color) {
+    if (!tex) {
+        tex = new TextureLoader(
+            await FileLoader.loadImg("img/nothing.png")
+        ).parse();
+    }
+
     return Importer.object({
         name: "star",
         transform: { position: pos, scale: [0.1, 0.1, 0.1] },
